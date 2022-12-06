b79a6e1e0a78dcf079d35583f063065ab6cf563d
armandosjunior <sidaarmando@gmail.com>
2022-12-06 01:41:09 -0700

    modified models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'product',
+        key: 'key'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
